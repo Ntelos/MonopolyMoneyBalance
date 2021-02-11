be465012f2fba7d87129e622712059e338c91c37
Panteleimon Kampouris <35311350+Ntelos@users.noreply.github.com>
2021-02-12 00:18:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 $(function(){
-
 	
+	var operation, result;
+
+	$("#button").click(function(){
+		
+		operation = $("#input").val();
+		
+		if(operation!=""){
+			
+			$('#tbody').append(`<tr><td>${operation}</td></tr>`);
+			
+			result = parseInt($('#balance').text(), 10) + parseInt(operation);
+			
+			$('#balance').text(result.toString());
+			
+			$("#input").val("");
+		
+		}
+		
+	});
 
 });
